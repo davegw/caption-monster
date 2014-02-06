90c541724faa4bfe0899e3b2bdab3728f7aabd20
davidgertmenianwong <david@giftrocket.com>
2014-02-05 19:58:52 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Caption::Application.config.secret_key_base = '83a333aeb1721c0ce0b34d3fd5cb471611ec295773a1ef38fd6a493516522c9776dbb503b0a8e44adec688e3a1164c7f2c8f4ca68af94c53cea814ac785099a9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Caption::Application.config.session_store :cookie_store, key: '_caption_session'
