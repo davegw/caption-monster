a992be86e3571a3400578fb494dc6eb0ca6462ea
davidgertmenianwong <david@giftrocket.com>
2014-02-09 15:45:17 -0800

    try changing up route

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,6 +2,8 @@ class Entry < ActiveRecord::Base
   belongs_to :user
   has_many :labels
   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
-                    :default_url => "/system/entries/photos/no_image.jpg"
+                    :default_url => "/system/entries/photos/no_image.jpg",
+                    :url => "/assets/:class/:attachment/:id_partition/:style/:filename",
+                    :path => ":rails_root/public/assets/:class/:attachment/:id_partition/:style/:filename"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
