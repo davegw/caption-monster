2ccc40befc909211e57181bf7eafaa78fdafe66c
davidgertmenianwong <david@giftrocket.com>
2014-02-09 15:58:16 -0800

    try storing images in assets

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -4,6 +4,6 @@ class Entry < ActiveRecord::Base
   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
                     :default_url => "/system/entries/photos/no_image.jpg",
                     :url => "/assets/:class/:attachment/:id_partition/:style/:filename",
-                    :path => ":rails_root/public/assets/:class/:attachment/:id_partition/:style/:filename"
+                    :path => ":rails_root/app/assets/images/:class/:attachment/:id_partition/:style/:filename"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
