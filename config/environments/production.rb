99c8a73bac8f6018b7fb2e6f8d08b46c5326b419
davidgertmenianwong <david@giftrocket.com>
2014-02-13 18:20:26 -0800

    fix syntax error again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,12 +81,12 @@ Caption::Application.configure do
   # Set up AWS S3 storage on Paperclip.
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_host_name => 's3-us-west-2.amazonaws.com',
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
-    :s3_host_name => 's3-us-west-2.amazonaws.com'
   }
 
 end
