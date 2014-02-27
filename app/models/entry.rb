b7588e7b7cfc7f2e9a4f3c21320c73b18deb3ba9
davidgertmenianwong <david@giftrocket.com>
2014-02-26 22:07:40 -0800

    set original image size

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,7 +1,7 @@
 class Entry < ActiveRecord::Base
   belongs_to :user
   has_many :labels, :dependent => :destroy
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>", :original => "1100x1100>" }, 
                     :convert_options => { :all => '-auto-orient' },
                     :default_url => "/system/entries/photos/no_image.jpg"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
